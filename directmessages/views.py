b6cb5544a6351133f629564f211ff56f9078f0fc
gabapppp <khiempham201@gmail.com>
2021-05-24 01:13:43 +0700

    abc

--- directmessages/views.py
+++ directmessages/views.py
@@ -1,22 +1,16 @@
 from users import views
 from django.dispatch.dispatcher import receiver
 from django_filters import filterset
-from rest_framework import serializers, viewsets, mixins, permissions
+from rest_framework import serializers, viewsets, mixins, permissions, response
 from .models import Message
 from .serializers import MessageSerializer
-import django_filters
-
-
-
-# This is create filterset.
-class MessageFilterSet(django_filters.FilterSet):
-    receiver = django_filters.CharFilter('receiver__username')
-    class Meta:
-        model = Message
-        fields = ['receiver',]
+from .filters import MessageFilterSet
+from django.contrib.auth.models import User
 
 # Create your views here.
-class MessageViewSet(viewsets.ReadOnlyModelViewSet, mixins.CreateModelMixin):
+
+
+class InboxViewSet(viewsets.ReadOnlyModelViewSet, mixins.CreateModelMixin):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filterset_class = MessageFilterSet
@@ -24,11 +18,18 @@ class MessageViewSet(viewsets.ReadOnlyModelViewSet, mixins.CreateModelMixin):
 
     def get_queryset(self):
         queryset = super().get_queryset()
+        
+        if self.action == 'list' \
+                and self.request.query_params.get('receiver', '').strip() == '' \
+                and self.request.query_params.get('inbox', '').strip() == '':
+            queryset = queryset.none()
+        elif not self.request.query_params.get('receiver', '').strip() == '':
+            queryset = queryset.filter(sender=self.request.user)
 
-        if self.action == 'list' and self.request.query_params.get('receiver', '').strip() == '':
-            return queryset.none()
+        return queryset
 
-        return queryset.filter(sender=self.request.user)
-    
     def perform_create(self, serializer):
-        serializer.save(sender = self.request.user)
+        serializer.save(
+            sender = self.request.user,
+            receiver = User.objects.filter(username=self.request.query_params.get('receiver', '').strip()).get()
+            )
