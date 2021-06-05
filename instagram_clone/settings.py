da681ecd22487248b79c168689ab7df7a53c788c
gabapppp <khiempham201@gmail.com>
2021-06-06 01:49:13 +0700

    main

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import django_heroku
 from django.conf import settings
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,6 +137,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static/'),
+
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
