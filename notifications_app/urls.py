08ad569f66425451eccae9c8808663469b8efe60
gabapppp <khiempham201@gmail.com>
2021-06-24 23:53:17 +0700

    main

--- notifications_app/urls.py
+++ notifications_app/urls.py
@@ -7,13 +7,13 @@ router.register('', NotificationViewSet)
 
 app_name = 'notifications_rest'
 urlpatterns = [
-    url(r'^all/', AllNotification.as_view({'get': 'list'}), name='all'),
-    url(r'^unread/', UnreadNotificationsList.as_view({'get': 'list'}), name='unread'),
-    url(r'^mark-all-as-read/$', MarkAllAsRead.as_view(), name='mark_all_as_read'),
-    url(r'^mark-as-read/(?P<slug>\d+)/$', MarkAsRead.as_view(), name='mark_as_read'),
-    url(r'^mark-as-unread/(?P<slug>\d+)/$', MarkAsUnread.as_view(), name='mark_as_unread'),
-    url(r'^delete/(?P<slug>\d+)/$', Delete.as_view(), name='delete'),
-    url(r'^api/unread_count/$', UnreadNotificationCount.as_view(), name='live_unread_notification_count'),
-    url(r'^api/all_count/$', AllNotificationCount.as_view(), name='live_all_notification_count'),
-    url(r'^api/unread_list/$', UnreadNotificationsList.as_view({'get': 'list'}), name='live_unread_notification_list'),
+    url('all/', AllNotification.as_view({'get': 'list'}), name='all'),
+    url('unread/', UnreadNotificationsList.as_view({'get': 'list'}), name='unread'),
+    url('mark-all-as-read/$', MarkAllAsRead.as_view(), name='mark_all_as_read'),
+    url('mark-as-read/(?P<slug>\d+)/$', MarkAsRead.as_view(), name='mark_as_read'),
+    url('mark-as-unread/(?P<slug>\d+)/$', MarkAsUnread.as_view(), name='mark_as_unread'),
+    url('delete/(?P<slug>\d+)/$', Delete.as_view(), name='delete'),
+    url('api/unread_count/$', UnreadNotificationCount.as_view(), name='live_unread_notification_count'),
+    url('api/all_count/$', AllNotificationCount.as_view(), name='live_all_notification_count'),
+    url('api/unread_list/$', UnreadNotificationsList.as_view({'get': 'list'}), name='live_unread_notification_list'),
 ]
